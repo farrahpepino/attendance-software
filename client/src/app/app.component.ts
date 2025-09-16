c35c97eda56f8774580ad1f0c4f9fa6b7712956c
farrahpepino <idcfarrah@gmail.com>
2025-09-16 19:47:56 -0400

    improved ui, planned backend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AuthComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
