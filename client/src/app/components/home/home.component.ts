3510cdb8abd4e27f8db0162a2c13551aed2abece
farrahpepino <idcfarrah@gmail.com>
2025-09-16 10:28:21 -0400

    improved UI

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent {
+  currentDate: Date = new Date();
+
+
   onSelect(event: Event){
     (event.target as HTMLSelectElement).selectedIndex = -1;
   }
