0e95ae424ffc0486d6b8f4a7fdbbb8ee5863b528
farrahpepino <idcfarrah@gmail.com>
2025-09-18 00:23:10 -0400

    code cleanup, improve ui

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 
+enum Active {
+  Absent = 'absent',
+  Present = 'present',
+}
 @Component({
   selector: 'app-home',
   imports: [NavbarComponent, CommonModule],
@@ -11,7 +15,17 @@ import { CommonModule } from '@angular/common';
 
 export class HomeComponent {
   currentDate: Date = new Date();
+  status: Active = Active.Present;
+  Active = Active;
 
+  toggleStatus(status: string){
+    if (status === 'present'){
+      this.status = Active.Present;
+    } 
+    else {
+      this.status = Active.Absent;
+    }
+  }
   onSelect(event: Event){
     (event.target as HTMLSelectElement).selectedIndex = -1;
   }
