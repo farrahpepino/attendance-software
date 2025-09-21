07bdc27e4f657d15818b130079af31607b41e62f
farrahpepino <idcfarrah@gmail.com>
2025-09-21 15:10:25 -0400

    improve ui

--- server/obj/Debug/net9.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net9.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+886329ca14403c8156076261dab014147a17e17b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bcac9e30b7de9a89875c590f4019c20303c6276")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
